5102b4c56b251e591b25253b6a3f25de8c28db0d
Cleo G. Wolf <wolfie141191@yahoo.com>
2012-10-16 15:49:51 -0400

    first commit

---
+++ third_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ThirdApp::Application.config.secret_token = 'ac1769905486e3d193bb69a34239b58a6859f80f6624c730093beeb2eec9e862d93edcc0aa615c2607b6e268ce9b13d3169cec88dde6ec1b7ef8545026ef68a6'
